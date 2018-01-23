deb97aa07736a273ce41a4a32a970ee9261a2755
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2018-01-23 03:12:19 +0100

    Update identified.js

--- identified.js
+++ identified.js
@@ -1,24 +1,4 @@
 Identified = {
-	unknown: [
-	27140,		// mystic heal from healing sphere XI
-	701510,		// mystic healing sphere XI
-	701520,		// mystic healing sphere XI
-	701530,		// mystic healing sphere XI
-	701550,		// mystic healing sphere XI
-	701590,		// mystic mana sphere
-	700408,		// mystic healing IX
-	4610,		// Teleportation from Friend list???
-	99020000,	// Teleportation from Friend list???
-	700100,		// Baff the mystic revival
-	700630,		// mystic aura
-	700730,		// mystic aura
-	700232,		// mystic aura
-	602,		// mystic aura
-	700330,		// mystic aura
-	701604,		// mystic healing totem I
-	801800,
-	801101
-	],
 	Debuff: [
 	000000001,	// This id's only for test, pls remove it later
 	000000010	// This id's only for test, pls remove it later
