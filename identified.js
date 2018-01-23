66d5d49c8f26fc38b63eda70522e97bc3b0ff21d
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2018-01-23 22:43:50 +0100

    Update id's

--- identified.js
+++ identified.js
@@ -1,6 +1,8 @@
 Identified = {
 	Debuff: [
-	000000001,	// This id's only for test, pls remove it later
-	000000010	// This id's only for test, pls remove it later
+	99001800,
+	99000580,
+	99001060, //Test
+	99000460  //Test
 	]
 };
